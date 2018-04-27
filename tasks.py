df037b793d1820830015450f1b3b64b8cdf1c270
bruke.cui <allanhboy@hotmail.com>
2018-04-27 16:47:09 +0800

    增加环境变量

--- tasks.py
+++ tasks.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
+import os
+
 from celery import Celery
 
-app = Celery('tasks')
+env_dict = os.environ
+broker = env_dict.get('BROKER', 'redis://localhost:6379/1')
+app = Celery('tasks', broker=broker)
+
 
 @app.task
 def crawl_keywords(what):
     print(what)
+
 
 if __name__ == '__main__':
     app.start()
