092a298a60c7bdf8b163872f5eebeb6bbb947807
bruke.cui <allanhboy@hotmail.com>
2018-04-27 17:00:42 +0800

    增加多线程处理

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@
 import os
 
 from celery import Celery
+from multiprocessing import Process
 
 env_dict = os.environ
 broker = env_dict.get('BROKER', 'redis://localhost:6379/1')
@@ -11,7 +12,12 @@ app = Celery('tasks', broker=broker)
 @app.task
 def crawl_keywords(what):
     print(what)
+    p = Process(target=crawl)
+    p.start()
+    p.join()
 
+def crawl():
+    print('============')
 
 if __name__ == '__main__':
     app.start()
