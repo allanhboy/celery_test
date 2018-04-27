6b50911ae129603ea7a03959f672600832613ec8
bruke.cui <allanhboy@hotmail.com>
2018-04-27 17:21:16 +0800

    增加billiard

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@
 import os
 
 from celery import Celery
-from multiprocessing import Process
+from billiard.pool import Pool
 
 env_dict = os.environ
 broker = env_dict.get('BROKER', 'redis://localhost:6379/1')
@@ -12,8 +12,9 @@ app = Celery('tasks', broker=broker)
 @app.task
 def crawl_keywords(what):
     print(what)
-    p = Process(target=crawl)
-    p.start()
+    p = Pool(3)
+    p.apply_async(crawl)
+    p.close()
     p.join()
 
 def crawl():
